471f2ba89a8c72657b7d31ecfddfd81abab56729
Juan <juanaditia94@gmail.com>
2021-07-14 21:06:05 +0700

    Twentycommit

--- app/src/main/java/com/example/juandaonlineshop/activity/RiwayatActivity.kt
+++ app/src/main/java/com/example/juandaonlineshop/activity/RiwayatActivity.kt
@@ -1,12 +1,19 @@
 package com.example.juandaonlineshop.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.juandaonlineshop.R
+import com.example.juandaonlineshop.adapter.AdapterProduk
+import com.example.juandaonlineshop.adapter.AdapterRiwayat
 import com.example.juandaonlineshop.app.ApiConfig
 import com.example.juandaonlineshop.helper.Helper
 import com.example.juandaonlineshop.helper.SharedPref
 import com.example.juandaonlineshop.model.ResponModel
+import com.example.juandaonlineshop.model.Transaksi
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_riwayat.*
 import kotlinx.android.synthetic.main.toolbar.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -31,10 +38,24 @@ class RiwayatActivity : AppCompatActivity() {
             override fun onResponse(call: Call<ResponModel>, response: Response<ResponModel>) {
                 val res = response.body()!!
                 if (res.success == 1) {
-
+                    displayRiwayat(res.transaksis)
                 }
             }
         })
+    }
+
+    fun displayRiwayat(transaksis: ArrayList<Transaksi>) {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        rv_riwayat.adapter = AdapterRiwayat(transaksis, object : AdapterRiwayat.Listeners {
+            override fun onClicked(data: Transaksi) {
+
+            }
+        })
+        rv_riwayat.layoutManager = layoutManager
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/example/juandaonlineshop/adapter/AdapterRiwayat.kt
@@ -0,0 +1,80 @@
+package com.example.juandaonlineshop.adapter
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.juandaonlineshop.R
+import com.example.juandaonlineshop.activity.DetailProdukActivity
+import com.example.juandaonlineshop.activity.LoginActivity
+import com.example.juandaonlineshop.helper.Helper
+import com.example.juandaonlineshop.model.Alamat
+import com.example.juandaonlineshop.model.Produk
+import com.example.juandaonlineshop.model.Transaksi
+import com.example.juandaonlineshop.util.Config
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class AdapterRiwayat(var data:ArrayList<Transaksi>, var listener: Listeners): RecyclerView.Adapter<AdapterRiwayat.Holder>() {
+
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvNama = view.findViewById<TextView>(R.id.tv_nama)
+        val tvHarga = view.findViewById<TextView>(R.id.tv_harga)
+        val tvTangal = view.findViewById<TextView>(R.id.tv_tgl)
+        val tvJumlah = view.findViewById<TextView>(R.id.tv_jumlah)
+        val tvStatus = view.findViewById<TextView>(R.id.tv_status)
+        val btnDetail = view.findViewById<TextView>(R.id.btn_detail)
+        val layout = view.findViewById<CardView>(R.id.layout)
+
+    }
+
+    lateinit var context: Context
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        context = parent.context
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_riwayat, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        val a = data[position]
+
+        val name = a.details[0].produk.name
+        holder.tvNama.text = name
+        holder.tvHarga.text = Helper().gantiRupiah(a.total_transfer)
+        holder.tvJumlah.text = a.total_item + " Items"
+        holder.tvStatus.text = a.status
+        holder.tvTangal.text = a.created_at
+
+        var color = context.getColor(R.color.menungu)
+        if (a.status == "SELESAI") color = context.getColor(R.color.selesai)
+
+
+        holder.tvStatus.setTextColor(color)
+
+        holder.layout.setOnClickListener {
+            listener.onClicked(a)
+        }
+    }
+
+    interface Listeners {
+        fun onClicked(data: Transaksi)
+    }
+
+}
